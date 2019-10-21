75ccbb94ebfdf52109070aebe3728fac4cdc3b0a
Xysto73 <48263050+Xysto73@users.noreply.github.com>
2019-10-21 08:38:14 -0300

    HTML e JS gerencia de departamentos
    Começo da criação da tabela para exibição de departamentos.

---
+++ src/js/diario/departamentos/departamentos.js
@@ -0,0 +1,6 @@
+//https://www.w3schools.com/xml/ajax_xmlfile.asp
+//https://www.w3schools.com/html/html_tables.asp
+function carregaDepartamentos() {
+  var mainEl = document.querySelector("#tabeladepartamentos");
+  var deptosXML =
+}
