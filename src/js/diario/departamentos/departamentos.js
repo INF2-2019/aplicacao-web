b9199df9c7c07af9b172cc02d84637a370d57766
Alface <pedrocesar1410@gmail.com>
2019-10-21 15:54:32 -0300

    Requisição XHR

--- src/js/diario/departamentos/departamentos.js
+++ src/js/diario/departamentos/departamentos.js
@@ -1,6 +1,15 @@
-//https://www.w3schools.com/xml/ajax_xmlfile.asp
-//https://www.w3schools.com/html/html_tables.asp
-function carregaDepartamentos() {
-  var mainEl = document.querySelector("#tabeladepartamentos");
-  var deptosXML =
+const servidor = "http://localhost:8080/app";
+
+function consultaDepartamentos() {
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if(this.readyState == 4 && this.status == 200) {
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(xhr.response, "application/xml");
+		}
+	};
+	xhr.open("POST", servidor  + "/diario/departamentos/consulta", true);
+	xhr.send();
 }
+
+consultaDepartamentos();
