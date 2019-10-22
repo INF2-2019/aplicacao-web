483b0db5ec197884dc86db63bd7b98814cf1fcc0
Xysto73 <48263050+Xysto73@users.noreply.github.com>
2019-10-22 18:56:26 -0300

    Update departamentos.js
    Criada funcao passatabela que cria a tabela de exibicao dos departamentos

--- src/js/diario/departamentos/departamentos.js
+++ src/js/diario/departamentos/departamentos.js
@@ -6,10 +6,28 @@ function consultaDepartamentos() {
 		if(this.readyState == 4 && this.status == 200) {
 			const parser = new DOMParser();
 			const doc = parser.parseFromString(xhr.response, "application/xml");
+			passaTabela(doc);
 		}
 	};
 	xhr.open("POST", servidor  + "/diario/departamentos/consulta", true);
 	xhr.send();
+}
+
+function passaTabela(doc) {
+	let tabela = document.querySelector('tbody');
+	let deptos = doc.getElementsByTagName('departamento');
+	console.log(deptos);
+	for(let depto of deptos){
+		console.log(depto);
+		let novarow = document.createElement("tr");
+		for(let x = 0; x<3; x++){
+			let novacell = document.createElement("td");
+			let textnode = document.createTextNode(depto.children[x].textContent);
+			novacell.appendChild(textnode)
+			novarow.appendChild(novacell)
+		}
+		tabela.appendChild(novarow);
+	}
 }
 
 consultaDepartamentos();
