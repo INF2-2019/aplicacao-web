ec5cb578ed8c182e741554f0bf97b15d07f800e0
Alface <pedrocesar1410@gmail.com>
2019-10-26 15:10:27 -0300

    Botoes adicionados
    Alteracoes feitas pelo Caio

--- src/js/diario/departamentos/departamentos.js
+++ src/js/diario/departamentos/departamentos.js
@@ -16,18 +16,55 @@ function consultaDepartamentos() {
 function passaTabela(doc) {
 	let tabela = document.querySelector('tbody');
 	let deptos = doc.getElementsByTagName('departamento');
-	console.log(deptos);
 	for(let depto of deptos){
-		console.log(depto);
 		let novarow = document.createElement("tr");
+		novarow.setAttribute("class", "criada");
 		for(let x = 0; x<3; x++){
 			let novacell = document.createElement("td");
 			let textnode = document.createTextNode(depto.children[x].textContent);
-			novacell.appendChild(textnode)
-			novarow.appendChild(novacell)
+			novacell.appendChild(textnode);
+			novarow.appendChild(novacell);
 		}
 		tabela.appendChild(novarow);
 	}
 }
 
+function addEditDelete() {
+	let linhas = document.getElementsByClassName("criada");
+	let newcell = document.createElement("td");
+	let editBut = document.createElement("button");
+	let delBut = document.createElement("button");
+	editBut.innerHTML = "Editar";
+	delBut.innerHTML = "Deletar";
+	editBut.setAttribute("type", "button");
+	delBut.setAttribute("type", "button");
+	editBut.setAttribute("onclick", "editarDepto()");
+	delBut.setAttribute("onclick", "deletarDepto()");
+	editBut.setAttribute("id", "botaoEdit");
+	delBut.setAttribute("id", "botaoDel");
+	newcell.appendChild(editBut);
+	newcell.appendChild(delBut);
+	for (let linha of linhas) {
+		linha.appendChild(newcell);
+	}
+	console.log(newcell);
+}
+
+function inserirDepto(){
+
+}
+
+function salvarDepto(){
+
+}
+
+function editarDepto() {
+
+}
+
+function deletarDepto() {
+
+}
+
 consultaDepartamentos();
+addEditDelete();
