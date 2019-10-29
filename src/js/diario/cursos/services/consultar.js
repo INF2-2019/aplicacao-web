b1b9230bf946a062b52473d29ba51903eb844fb7
Arthur Guerra <conta.acg2002@gmail.com>
2019-10-29 12:31:58 -0300

    Página de manutenção de cursos

---
+++ src/js/diario/cursos/services/consultar.js
@@ -0,0 +1,7 @@
+// TODO: adicionar consultas e pesquisas seletivas
+function consultar() {
+	fetch(baseURL + 'consultar')
+		.then(response => response.text())
+		.then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+		.then(xml => criaTabela(xml))
+}
---
+++ src/js/diario/cursos/services/deletar.js
@@ -0,0 +1,13 @@
+function deletar() {
+	fetch(baseURL + 'deletar?id=' + currentId)
+		.then(response => response.text())
+		.then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+		.then(xml => (retornaResposta(xml)))
+		.then(resposta => {
+			if (resposta == "Deletado com sucesso.") {
+				M.toast({ html: resposta, classes: 'utils sucesso-2 text-light-text' })
+			} else {
+				M.toast({ html: resposta, classes: 'utils erro-2 text-light-text' })
+			}
+		}).then(() => consultar())
+}
