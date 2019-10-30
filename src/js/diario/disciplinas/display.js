fb5a2d34dde7a9e8958302dcfbb20dc3cb46777b
MarcusMoraes <marcussviniciusss123@gmail.com>
2019-10-29 23:22:48 -0300

    script que reseta os inputs de inserir e alterar

---
+++ src/js/diario/disciplinas/display.js
@@ -0,0 +1,10 @@
+M.AutoInit();
+
+function limpaInputs() {
+	document.getElementById('AdicionaCargaDis').value = "";
+	document.getElementById('AdicionaNomeDis').value = "";
+	document.getElementById('AdicionarTurmaDis').value = "";
+	document.getElementById('editaTurmaDis').value = "";
+	document.getElementById('editaNomeDis').value = "";
+	document.getElementById('editaCargaDis').value = "";
+}
