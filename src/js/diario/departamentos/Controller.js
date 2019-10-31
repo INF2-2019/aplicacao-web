53b4ea27a4890e34a178c24a2c6e6c84138238ea
Alface <pedrocesar1410@gmail.com>
2019-10-30 21:49:21 -0300

    Operacoes de insercao, atualizacao e remocao

--- src/js/diario/departamentos/Controller.js
+++ src/js/diario/departamentos/Controller.js
@@ -1,49 +1,91 @@
 class Controller {
 
-    static local = "http://localhost:8080/app/diario/departamentos/";
+	static local = "http://localhost:8080/app/diario/departamentos/";
 
-    static consulta() {
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if(this.readyState == 4 && this.status == 200) {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(xhr.response, "application/xml");
-                const deptos = View.deptos(doc);
-                Tabela.limpa();
-                for(let depto of deptos) {
-                    Tabela.insere(depto);
-                }
-            }
-        };
-        const url = Controller.local + "consulta";
-        xhr.open("GET", url, true);
-        xhr.send();
-    }
+	static consulta() {
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				const parser = new DOMParser();
+				const doc = parser.parseFromString(xhr.response, "application/xml");
+				const erro = doc.getElementsByTagName("erro")[0];
+				if(!erro) {
+					const deptos = View.deptos(doc);
+					Tabela.limpa();
+					for(let depto of deptos) {
+						Tabela.insere(depto);
+					}
+				} else {
+					View.erro(erro);
+				}
+			}
+		};
+		const url = Controller.local + "consulta";
+		xhr.open("GET", url, true);
+		xhr.send();
+	}
 
-    static adicionarDepto(){
-        const depto = {
-            id: Tabela.tbody.children.length+1,
-            idCampi: document.querySelector('#campus_input').value,
-            nome: document.querySelector('#name_input').value
-        };
-        Tabela.insere(depto);
-        const xhr = new XMLHttpRequest();
-        const url = Controller.local + "insere";
-        const enviado = "?id-campi="+depto.idCampi+"&nome="+depto.nome;
-        xhr.open("GET", url + enviado, true);
-        xhr.send();
-    }
-    
-    static deletaDepto(id){
-        const xhr = new XMLHttpRequest();
-        const url = Controller.local + "remove";
-        const enviado = "?id="+document.getElementById(id).getAttribute("Id");
-        xhr.open("GET", url + enviado, true);
-        xhr.send();
-        Tabela.tbody.removeChild(document.getElementById(id));
-    }
-    static editaDepto(id){
+    static insere(depto) {
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				const parser = new DOMParser();
+				const doc = parser.parseFromString(xhr.response, "application/xml");
+				const sucesso = doc.getElementsByTagName("sucesso")[0],
+					erro = doc.getElementsByTagName("erro")[0];
+				if(sucesso)
+					View.sucesso(sucesso);
+				else
+					View.erro(erro);
+				Controller.consulta();
+			}
+		};
+		const params = "?id-campi=" + depto.idCampi + "&nome=" + depto.nome;
+		const url = Controller.local + "insere" + params;
+		xhr.open("POST", url, true);
+		xhr.send();
+	}
 
-    }
+	static atualiza(depto) {
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				const parser = new DOMParser();
+				const doc = parser.parseFromString(xhr.response, "application/xml");
+				const sucesso = doc.getElementsByTagName("sucesso")[0],
+					erro = doc.getElementsByTagName("erro")[0];
+				if(sucesso)
+					View.sucesso(sucesso);
+				else
+					View.erro(erro);
+				Controller.consulta();
+			}
+		};
+		const params = "?id=" + depto.id + "&id-campi=" + depto.idCampi + "&nome=" + depto.nome;
+		const url = Controller.local + "atualiza" + params;
+		xhr.open("POST", url, true);
+		xhr.send();
+	}
+
+	static remove(id) {
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				const parser = new DOMParser();
+				const doc = parser.parseFromString(xhr.response, "application/xml");
+				const sucesso = doc.getElementsByTagName("sucesso")[0],
+					erro = doc.getElementsByTagName("erro")[0];
+				if(sucesso)
+					View.sucesso(sucesso);
+				else
+					View.erro(erro);
+				Controller.consulta();
+			}
+		};
+		const params = "?id=" + id;
+		const url = Controller.local + "remove" + params;
+		xhr.open("POST", url, true);
+		xhr.send();
+	}
 
 }
--- src/js/diario/departamentos/View.js
+++ src/js/diario/departamentos/View.js
@@ -1,77 +1,79 @@
 class View {
-    static numerolinhas = 0;
-    static deptos(doc) {
-        const deptos = [];
-        const docDeptos = doc.getElementsByTagName('departamento');
-        for(let docDepto of docDeptos) {
-            const depto = {
-                id:  docDepto.children[0].textContent,
-                idCampi: docDepto.children[1].textContent,
-                nome: docDepto.children[2].textContent
-            };
-            deptos.push(depto);
-        }
-        return deptos;
-    }
 
-    static linhaTabela(depto) {
-        const tr = document.createElement("tr");
-        View.numerolinhas++;
-        tr.setAttribute("id", View.numerolinhas)
-        tr.appendChild(View.celulaTexto(depto.nome));
-        tr.appendChild(View.celulaTexto(depto.idCampi));
-        tr.appendChild(View.celulaAcoes());
-        return tr;
-    }
+	static deptos(doc) {
+		const deptos = [];
+		const docDeptos = doc.getElementsByTagName('departamento');
+		for(let docDepto of docDeptos) {
+			const depto = {
+				id:  parseInt(docDepto.children[0].textContent),
+				idCampi: parseInt(docDepto.children[1].textContent),
+				nome: docDepto.children[2].textContent
+			};
+			deptos.push(depto);
+		}
+		return deptos;
+	}
 
-    static celulaTexto(txt) {
-        const td = document.createElement("td");
-        const txtNode = document.createTextNode(txt);
-        td.appendChild(txtNode);
-        return td;
-    }
+	static linhaTabela(depto) {
+		const tr = document.createElement("tr");
+		tr.appendChild(View.celulaTexto(depto.nome));
+		tr.appendChild(View.celulaTexto(depto.idCampi));
+		tr.appendChild(View.celulaAcoes(depto.id));
+		return tr;
+	}
 
-    static celulaAcoes() {
-        const td = document.createElement("td");
-        td.appendChild(View.botaoInfo());
-        td.appendChild(View.botaoEditar());
-        td.appendChild(View.botaoDeletar());
-        return td;
-    }
+	static celulaTexto(txt) {
+		const td = document.createElement("td");
+		const txtNode = document.createTextNode(txt);
+		td.appendChild(txtNode);
+		return td;
+	}
 
-    static botaoInfo() {
-        const button = document.createElement("button");
-        button.classList.add("btn");
-        button.classList.add("modal-trigger");
-        button.setAttribute("data-target", "modal-info");
-        button.style.backgroundColor = "#00B0FF";
-        button.style.margin = "0 5px";
-        button.innerText = "INFO";
-        return button;
-    }
+	static celulaAcoes(id) {
+		const td = document.createElement("td");
+		td.classList.add("right");
+		td.appendChild(View.botaoEditar(id));
+		td.appendChild(View.botaoExcluir(id));
+		return td;
+	}
 
-    static botaoEditar() {
-        const button = document.createElement("button");
-        button.classList.add("btn");
-        button.classList.add("modal-trigger");
-        button.setAttribute("id", View.numerolinhas)
-        button.setAttribute("onclick", "Controller.editaDepto("+View.numerolinhas+")");
-        button.setAttribute("data-target", "modal-editar");
-        button.style.margin = "0 5px";
-        button.innerText = "EDITAR";
-        return button;
-    }
+	static botaoEditar(id) {
+		const button = document.createElement("button");
+		button.classList.add("btn");
+		button.classList.add("modal-trigger");
+		button.onclick = function() {
+			idEditar = id;
+		}
+		button.setAttribute("data-target", "modal-editar");
+		button.style.margin = "0 5px";
+		button.innerText = "EDITAR";
+		return button;
+	}
 
-    static botaoDeletar() {
-        const button = document.createElement("button");
-        button.classList.add("btn");
-        button.setAttribute("id", View.numerolinhas)
-        button.setAttribute("onclick", "Controller.deletaDepto("+View.numerolinhas+")");
-        button.style.backgroundColor = "#E53935";
-        button.style.margin = "0 5px";
-        button.innerText = "DELETAR";
-        return button;
-    }
+	static botaoExcluir(id) {
+		const button = document.createElement("button");
+		button.classList.add("btn");
+		button.onclick = function() {
+			Controller.remove(id);
+		}
+		button.style.backgroundColor = "#E53935";
+		button.style.margin = "0 5px";
+		button.innerText = "EXCLUIR";
+		return button;
+	}
+
+	static sucesso(doc) {
+		const toast = document.querySelector("#toast");
+		toast.style.backgroundColor = "#00E676";
+		toast.innerHTML = doc.children[0].textContent;
+	}
+
+	static erro(doc) {
+		const toast = document.querySelector("#toast");
+		toast.style.backgroundColor = "#D32F2F";
+		toast.innerHTML = doc.children[0].textContent;
+	}
+
 }
 
     
