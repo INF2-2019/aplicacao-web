20d647b500620f83bb047790f4c1a736711c278a
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2019-11-07 08:04:12 -0300

    Usando constantes em editar.js

--- src/js/diario/professores/editar.js
+++ src/js/diario/professores/editar.js
@@ -1,6 +1,8 @@
 const paramNomes = ["id", "id-depto", "nome", "senha", "email", "titulacao"];
 const ROTA_EDICAO = "diario/professores/atualizar";
 const ROTA_REMOCAO = "diario/professores/deletar";
+const ROTA_CONSULTA = "diario/professores/consultar";
+const LABEL_ATRIBUTOS = 'class="primary-text text-lighten-1 short col s12" style="font-size:18px;"'
 
 function info(id){
 	if(id == ""){
@@ -9,37 +11,36 @@ function info(id){
 	}
 
 	let xhttp = new XMLHttpRequest();
-	let url = ENDERECO + "diario/professores/consultar?id=" + id;
+	let url = ENDERECO + ROTA_CONSULTA + "?id=" + id;
 
 	xhttp.open("GET", url, true);
 	xhttp.onreadystatechange = function() {
 		if(xhttp.readyState === xhttp.DONE && xhttp.status === 200) {
-                        var xml = (new DOMParser()).parseFromString(this.responseText, "application/xml");
+				var xml = (new DOMParser()).parseFromString(this.responseText, "application/xml");
 			var raiz = xml.firstElementChild;
 			if(raiz.nodeName == "erro") {
 				window.alert(raiz.firstElementChild.textContent);
 				return;
 			}else{
-                            var elementos = raiz.children;
-                            let informacoes = document.getElementById("informacoes");
-			informacoes.innerHTML = "";
-			for(let i = 0; i < elementos.length; i++) {
-				var dados = elementos[i].children;
-				informacoes.innerHTML += "<label class=\"primary-text text-lighten-1 short col s12\" style=\"font-size:18px;\" id=\"id\"> SIAPE: " + dados[0].textContent + "</label>";
-				informacoes.innerHTML += "<label class=\"primary-text text-lighten-1 short col s12\" style=\"font-size:18px;\" id=\"nome\"> Nome: " + dados[1].textContent + "</label>";
-                                informacoes.innerHTML += "<label class=\"primary-text text-lighten-1 short col s12\" style=\"font-size:18px;\" id=\"idDepto\"> id-Depto: " + dados[2].textContent + "</label>";
-                                informacoes.innerHTML += "<label class=\"primary-text text-lighten-1 short col s12\" style=\"font-size:18px;\" id=\"email\"> E-mail: " + dados[3].textContent + "</label>";
-                                informacoes.innerHTML += "<label class=\"primary-text text-lighten-1 short col s12\" style=\"font-size:18px;\" id=\"titulacao\"> Titulação: " + dados[4].textContent + "</label>";
+				var elementos = raiz.children;
+				let informacoes = document.getElementById("informacoes");
+				informacoes.innerHTML = "";
+				for(let i = 0; i < elementos.length; i++) {
+					var dados = elementos[i].children;
+					informacoes.innerHTML += "<label " + LABEL_ATRIBUTOS + " id=\"id\"> SIAPE: " + dados[0].textContent + "</label>";
+					informacoes.innerHTML += "<label " + LABEL_ATRIBUTOS + " id=\"nome\"> Nome: " + dados[1].textContent + "</label>";
+					informacoes.innerHTML += "<label " + LABEL_ATRIBUTOS + " id=\"idDepto\"> id-Depto: " + dados[2].textContent + "</label>";
+					informacoes.innerHTML += "<label " + LABEL_ATRIBUTOS + " id=\"email\"> E-mail: " + dados[3].textContent + "</label>";
+					informacoes.innerHTML += "<label " + LABEL_ATRIBUTOS + " id=\"titulacao\"> Titulação: " + dados[4].textContent + "</label>";
+				}
 			}
-                            //atualizarTabela();
-                        }
 		}
 	};
 	xhttp.send();
 }
 
 function deletar(id) {
-	
+
 	if(id == ""){
 		window.alert("Insira ID");
 		return;
@@ -67,10 +68,10 @@ function deletar(id) {
 }
 
 function alterar() {
-        
+
 	let xhttp = new XMLHttpRequest();
 
-	
+
 	let id = document.getElementsByName("id")[0].value;
 	let idDepto = document.getElementsByName("id-depto")[0].value;
 	let nome = document.getElementsByName("nome")[0].value;
@@ -78,11 +79,11 @@ function alterar() {
 	let email = document.getElementsByName("email")[0].value;
 	let titulacao = document.getElementsByName("titulacao")[0].value;
 
-	jqueryAjax(id, idDepto, nome, senha, email, titulacao);
+	requisicaoPost(id, idDepto, nome, senha, email, titulacao);
 }
 
-
-function jqueryAjax(id, idDepto, nome, senha, email, titulacao) {
+// Não consegui fazer o vanilla funcionar com POST, sinto muito
+function requisicaoPost(id, idDepto, nome, senha, email, titulacao) {
 	$.ajax(ENDERECO+ROTA_EDICAO, {
 		method: "POST",
 		data: {
@@ -101,7 +102,7 @@ function jqueryAjax(id, idDepto, nome, senha, email, titulacao) {
 
 		function fail(data, status) {
 			let resposta = data.responseXML.firstElementChild.firstElementChild.innerHTML;
-			document.getElementsByTagName("p")[0].innerHTML = resposta;
+			document.getElementsByTagName("p")[0].innerHTML = status == 404 ? "404... Servidor offline?" : resposta;
 		}
 	);
 }
