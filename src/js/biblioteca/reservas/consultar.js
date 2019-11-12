8e7788cf75c67bffa2f61b5661acae7427bc8ee4
Wagner Alexsander <wagnera.cviana@gmail.com>
2019-11-12 14:57:50 -0300

    Login adm e navbars

--- src/js/biblioteca/reservas/consultar.js
+++
@@ -1,6 +0,0 @@
-function consultar() {
-	fetch(baseURL + 'consultar')
-		.then(response => response.text())
-		.then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-		.then(xml => criaTabela(xml))
-}
