86efd0f8974931bf047624da9cc048c707d83a24
Pedro <pedro.gandra.andrade@gmail.com>
2019-11-12 12:03:21 -0300

    Manutenção de campi
    - Completa
    - CSS Corrigido
    
    Obs: criei a branch dnv pq tinha duplicado mts commits sem querer

---
+++ src/js/diario/campi/inserir.js
@@ -0,0 +1,27 @@
+var modal = document.getElementById("modal-cadastro");
+var btn = document.getElementById("botaoAdicionar");
+
+function inserir() {
+  let inpNome = document.querySelector("#nome");
+  let inpCidade = document.querySelector("#cidade");
+  let inpUf = document.querySelector("#uf");
+
+  let nome = document.querySelector("#nome").value;
+  let cidade = document.querySelector("#cidade").value;
+  let uf = document.querySelector("#uf").value;
+
+  let url = "http://localhost:8080/app/diario/campi/inserir?nome="+nome
+  +"&cidade="+cidade
+  +"&uf="+uf;
+  fetch(url)
+    .then(resposta => {
+      responseStatus = resposta.status;
+      return resposta.text();
+    })
+    .then(text => {
+      parser = new DOMParser();
+      xmlDoc = parser.parseFromString(text, "text/xml");
+    });
+    setTimeout(function(){ listar(); }, 200);
+    setTimeout(function(){ adicionaResult(responseStatus, xmlDoc); }, 200);
+}
