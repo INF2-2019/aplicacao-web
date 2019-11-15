ace3f01e3c33901c917366841040a135ea5a0404
Wagner Alexsander <wagnera.cviana@gmail.com>
2019-11-15 18:20:29 -0300

    navbars finalizadas(acho)

---
+++ src/js/biblioteca/alunos/deletar.js
@@ -0,0 +1,38 @@
+var btDeletar;
+let idParaDeletar;
+let vetDeletar;
+
+function listenerDelet(e) {
+  idParaDeletar = (e.currentTarget.parentNode.previousSibling.previousSibling.previousSibling.innerHTML);
+}
+
+function prepareDeletar() {
+
+btDeletar = document.getElementById("delet");
+btDeletar.addEventListener("click", deletar);
+
+
+  vetDeletar = document.querySelectorAll(".botaoDeletar");
+
+  for (var i = 0; i < vetDeletar.length; i++) {
+    vetDeletar[i].addEventListener("click", listenerDelet, true);
+  }
+}
+
+
+function deletar() {
+
+
+let url = "http://localhost:8080/app/biblioteca/alunos/deletar?id="+idParaDeletar;
+fetch(url, { credentials: 'include' })
+  .then(resposta => {
+    responseStatus = resposta.status;
+    return resposta.text();
+  })
+  .then(text => {
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(text, "text/xml");
+    listar();
+    adicionaResult(responseStatus, xmlDoc);
+  });
+}
--- src/js/diario/navbars/navbar.js
+++ src/js/diario/navbars/navbar.js
@@ -1,35 +1,106 @@
-const METODO = "POST";
 const ENDERECO = "http://localhost:38695/app";
 const ROTA = "/diario/cargo";
 
 let ul_dropdown;
 let ul_itens;
+let li_dropdown = [];
+let li_itens = [];
+let ul_funcoes;
 
-function navCONVIDADO() {
+function navADMIN() {
     ul_dropdown = document.getElementById("dropdown1");
-    ul_itens = document.getElementById("navbars-itens");
+	ul_itens = document.getElementById("navbars-itens");
+	ul_funcoes = document.getElementById("nav-funcoes");
 
-	let li_criado = document.createElement("li");
-	li_criado.innerHTML = "<li><a href='#' >Perfil do Aluno</a></li>";
-	ul_itens.appendChild(li_criado);
+	for(i=0;i<2;i++){
+		li_itens[i] = document.createElement("li");
+	}
+	li_itens[0].innerHTML = "<a href='../login-adm.html'>Login</a>";
+	li_itens[1].innerHTML = "<a href='../transicao/adm.html'>Página Inicial</a>";
+	$(".dropdown-trigger").dropdown();
+	for(i=0;i<li_itens.length;i++){
+		ul_itens.insertBefore(li_itens[i],ul_funcoes);
+	}
+
+	for(i=0;i<8;i++){
+		li_dropdown[i] = document.createElement("li");
+	}
+	li_dropdown[0].innerHTML = "<a href='../alunos/index.html'>Alunos</a>";
+	li_dropdown[1].innerHTML = "<a href='../campi/index.html'>Campi</a>";
+	li_dropdown[2].innerHTML = "<a href='../cursos/index.html'>Cursos</a>";
+	li_dropdown[3].innerHTML = "<a href='../departamentos/index.html'>Departamentos</a>";
+	li_dropdown[4].innerHTML = "<a href='../disciplinas/index.html'>Disciplinas</a>";
+	li_dropdown[5].innerHTML = "<a href='../etapas/index.html'>Etapas</a>";
+	li_dropdown[6].innerHTML = "<a href='../professores/index.html'>Professores</a>";
+	li_dropdown[7].innerHTML = "<a href='../transferencia/index.html'>Transferência de alunos</a>";
+	for(i=0;i<li_dropdown.length;i++){
+		ul_dropdown.appendChild(li_dropdown[i]);
+	}
+}
+
+function navALUNO() {
+	ul_dropdown = document.getElementById("dropdown1");
+	ul_itens = document.getElementById("navbars-itens");
+	ul_funcoes = document.getElementById("nav-funcoes");
+
+	for(i=0;i<2;i++){
+		li_itens[i] = document.createElement("li");
+	}
+	li_itens[0].innerHTML = "<a href='../login-aluno.html'>Login</a>";
+	li_itens[1].innerHTML = "<a href='../transicao/aluno.html'>Página Inicial</a>";
+	$(".dropdown-trigger").dropdown();
+	for(i=0;i<li_itens.length;i++){
+		ul_itens.insertBefore(li_itens[i],ul_funcoes);
+	}
+
+	for(i=0;i<4;i++){
+		li_dropdown[i] = document.createElement("li");
+	}
+	li_dropdown[0].innerHTML = "<a href='../relatorios/#'>Certificado de Escolaridade</a>";
+	li_dropdown[1].innerHTML = "<a href='../diario/telainicialdiarioaluno.html'>Diário Escolar</a>";
+	li_dropdown[2].innerHTML = "<a href='../relatorios/#'>Histórico Escolar</a>";
+	li_dropdown[3].innerHTML = "<a href='../alunos/perfil.html'>Perfil do Aluno</a>";
+	for(i=0;i<li_dropdown.length;i++){
+		ul_dropdown.appendChild(li_dropdown[i]);
+	}
+}
+
+function navPROFESSOR() {
+	ul_dropdown = document.getElementById("dropdown1");
+	ul_itens = document.getElementById("navbars-itens");
+	ul_funcoes = document.getElementById("nav-funcoes");
+
+	for(i=0;i<2;i++){
+		li_itens[i] = document.createElement("li");
+	}
+	li_itens[0].innerHTML = "<a href='../login-professor.html'>Login</a>";
+	li_itens[1].innerHTML = "<a href='../transicao/professor.html'>Página Inicial</a>";
+	$(".dropdown-trigger").dropdown();
+	for(i=0;i<li_itens.length;i++){
+		ul_itens.insertBefore(li_itens[i],ul_funcoes);
+	}
+
+	for(i=0;i<2;i++){
+		li_dropdown[i] = document.createElement("li");
+	}
+	li_dropdown[0].innerHTML = "<a href='../diario/telainicialdiarioprofessor.html'>Diário Escolar</a>";
+	li_dropdown[1].innerHTML = "<a href='../professores/perfil.html'>Perfil do professor</a>";
+	for(i=0;i<li_dropdown.length;i++){
+		ul_dropdown.appendChild(li_dropdown[i]);
+	}
 }
 
 function cargoLogado() {
 	$.ajax(ENDERECO+ROTA, {
 		xhrFields: { withCredentials: true }
 	})
-	.then(
-		function success(cargo) {
-			str = new XMLSerializer().serializeToString(cargo.firstChild);
-			if(str.substr("CONVIDADO")){
-				navCONVIDADO();
-			}
-		},
-
-		function fail() {
-            return("erro");
-		}
-	);
+	.then(response => {
+		str = response.getElementsByTagName("cargo")[0];
+		cargo = str.childNodes[0].nodeValue;
+		if(cargo=="ADMIN")navADMIN();
+		else if(cargo=="ALUNO")navALUNO();
+		else if(cargo=="PROFESSOR")navPROFESSOR();
+		else return;
+	})
+	.catch(error => console.error(error));
 }
-
-$(".dropdown-trigger").dropdown();
